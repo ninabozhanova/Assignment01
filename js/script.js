628ccc6d100e25c301ececa617b847129d711770
Nina Bozhanova <nbozhanova@gmail.com>
2024-02-18 19:12:35 -0800

    Finished assignment

---
+++ js/script.js
@@ -0,0 +1,144 @@
+// STEP 1
+
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// STEP 2
+
+// 18
+// "February"
+// true
+// null
+
+// STEP 3
+
+// let theGreeting = "Hello!"
+// let theSum = 1 + 1
+
+// STEP 4
+
+// let firstName
+// let lastName
+// let strAddress
+// let strCity
+// let strState
+// let zipCode
+// let yourAge
+// let referralSource
+// let blnMayWeContactYou
+
+// STEP 5
+
+// let firstName
+// let strState
+// let blnMayWeContactYou
+// firstName = "Nina"
+// strState = "CA"
+// blnMayWeContactYou = true
+
+// let firstName = "Nina"
+// let strState = "CA"
+// let blnMayWeContactYou = true
+
+// let firstName = "Nina", strState = "CA", blnMayWeContactYou = true
+
+// STEP 6
+
+// let theMessage = 42 + ' is the Answer to the Ultimate Question of Life, the Universe, and Everything'
+// console.log(theMessage)
+
+// STEP 7
+
+// let firstVariable = "This is the result of adding 18 to " + true
+// console.log(firstVariable)
+
+// let secondVariable = true + 18
+// console.log(secondVariable)
+
+// STEP 8
+
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// STEP 9
+
+// let aVariable = null
+// console.log(aVariable)
+
+// let anotherVariable
+// console.log(anotherVariable)
+
+// STEP 10
+
+// console.log(typeof "February")
+// console.log(typeof 18)
+// console.log(typeof true)
+// console.log(typeof [])
+// console.log(typeof a)
+
+// STEP 11
+
+// alert('Hello ' + 'Nina' + ', welcome to the JavaScript class!')
+
+// STEP 12
+
+// let name = "Nina"
+// alert('Hello ' + name + ', welcome to the JavaScript class!')
+
+// STEP 13
+
+// let course = 'JavaScript'
+// alert(`Hello Nina, welcome to the ${course} class!`)
+
+// STEP 14
+
+// alert('Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!')
+
+// STEP 15
+
+// let name = prompt('Enter your name')
+// alert('Hello ' + name + ', welcome to the JavaScript class!')
+
+// STEP 16
+
+// let course = prompt('Enter the name of the class you are taking')
+// alert(`Hello Nina, welcome to the ${course} class!`)
+
+// STEP 17
+
+// let x = 10
+// let y = 20
+// console.log(x + y)
+
+// STEP 18
+
+// let x = 20
+// x += 20
+// console.log(x)
+
+// STEP 19
+
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// STEP 20
+
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+
+// STEP 21
+
+// let x = 7
+// let y = 5
+// console.log(x != 10 || x > y)
+
+// STEP 22
+
+// let x = 42
+// let y = "42"
+// console.log(x == y && x === y)
